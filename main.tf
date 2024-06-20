d1e5e422486598cf0a97b009f00b1a2fb31ee73c
giladk123 <giladk@gmail.com>
2024-06-20 09:57:25 +0300

    added default value for soft_delete_retention_days

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_key_vault" "kv" {
     location                      = each.value.location
     resource_group_name           = each.value.resource_group_name
     tenant_id                     = each.value.tenant_id
-    soft_delete_retention_days    = each.value.soft_delete_retention_days
+    soft_delete_retention_days    = lookup(each.value, "soft_delete_retention_days", "") != "" ? tonumber(each.value.soft_delete_retention_days) : 7
     purge_protection_enabled      = each.value.purge_protection_enabled
     sku_name                      = each.value.sku_name
     public_network_access_enabled = each.value.public_network_access_enabled
