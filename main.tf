2614b5bc206a48a5caa0c72e8616112a1b95a67c
giladk123 <giladk@gmail.com>
2024-06-22 00:03:56 +0300

    removed soft_delete default and added purge protection enabled default to true

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "azurerm_key_vault" "kv" {
     location                      = each.value.location
     resource_group_name           = each.value.resource_group_name
     tenant_id                     = each.value.tenant_id
-    soft_delete_retention_days    = lookup(each.value, "soft_delete_retention_days", "") != "" ? tonumber(each.value.soft_delete_retention_days) : 7
-    purge_protection_enabled      = each.value.purge_protection_enabled
+    soft_delete_retention_days    = each.value.soft_delete_retention_days
+    purge_protection_enabled      = true
     sku_name                      = each.value.sku_name
     public_network_access_enabled = each.value.public_network_access_enabled
 }
